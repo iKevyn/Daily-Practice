433398a0ce7ab7a9f4e0b3195a9ecbfbaccc814b
Kevyn Jesus <jesusmonroe2019@gmail.com>
2023-12-07 16:42:03 -0300

    Editando arquivo que criei no github primeiro

---
+++ Operadores aritmeticos.js
@@ -0,0 +1,15 @@
+/*
+Declare duas variáveis, numero1 e numero2, e atribua a elas valores numéricos. 
+Utilize operadores aritméticos para realizar operações como adição, subtração, multiplicação ou divisão.
+*/
+
+
+const numero1 = 1;
+const numero2 = 2;
+
+let adição = numero1 + numero2;
+let subtração = numero1 - numero2;
+let divisão = numero1 / numero2;
+let multiplicação = numero1 * numero2;
+
+console.log(adição, subtração, divisão, multiplicação);
