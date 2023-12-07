b5396bb0d953a892aa001983208c4a607ec1b4dd
Kevyn Jesus <jesusmonroe2019@gmail.com>
2023-12-07 17:03:48 -0300

    Renomeando o nome do codigo

---
+++ Cumprimentar o Usuario.js
@@ -0,0 +1,14 @@
+/* 
+First time saving my codes in the github
+*/
+
+function cumprimentarUsuario(frase) {
+    if (frase === "Olá") {
+        console.log("Olá, tudo bem?")
+    } else {
+        console.log("Tchau, até mais!");
+    }
+
+}
+
+cumprimentarUsuario("Tchau");
