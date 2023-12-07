4c50e9d042983ee5268c830cb8ea61ca4b82f40a
Kevyn Jesus <jesusmonroe2019@gmail.com>
2023-12-07 17:27:25 -0300

    Primeiro commit

---
+++ Calculando Custo Compra.js
@@ -0,0 +1,13 @@
+/*/
+Declare uma variável chamada nomeProduto e atribua o nome de um produto.
+Declare uma variável chamada precoProduto e atribua o preço desse produto.
+Declare uma variável chamada quantidadeComprada e atribua a quantidade desse produto que uma pessoa comprou.
+Calcule o custo total da compra e imprima no console uma mensagem como "Você comprou X unidades do produto Y, o custo total é Z reais".
+/*/
+
+let nomeProduto = "Café";
+let precoProduto = 12;
+let quantidadeComprada= 3;
+let valorTotal = precoProduto * quantidadeComprada;
+
+console.log("Você comprou " + quantidadeComprada + " unidades do produto " + nomeProduto + ", o custo total é " + valorTotal + " reais.");
---
+++ Verificando a idade.js
@@ -0,0 +1,12 @@
+/*/Declare uma variável chamada idadeUsuario e atribua a idade de um usuário.
+Crie uma estrutura condicional para verificar se o usuário é maior de 18 anos.
+Imprima no console mensagens diferentes dependendo da condição, por exemplo, "Bem-vindo ao site" ou "Você precisa ser maior de idade para acessar este site"./*/
+
+let idadeUsuario = 20;
+let idadeNecessária = 18;
+
+if (idadeUsuario >= idadeNecessária) {
+console.log ("Bem-vindo ao site")
+} else { 
+console.log("Você precisa ser maior de idade para acessar este site")
+}
