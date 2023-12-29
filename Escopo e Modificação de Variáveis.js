152392a9f9cf96c0aef77f05e23d2a9a8540da1f
Kevyn Jesus <kevyn.contacts@gmail.com>
2023-12-29 15:56:19 -0300

    exercice

---
+++ Escopo e Modificação de Variáveis.js
@@ -0,0 +1,14 @@
+/*
+
+Crie uma função chamada dobrar que dobra o valor de uma variável global chamada numero. 
+Em seguida, chame a função e imprima o valor atualizado de numero.
+
+*/
+let numero = 50;
+
+function dobrar() {
+    numero = numero * 2;
+    console.log(numero);
+}
+
+dobrar();
