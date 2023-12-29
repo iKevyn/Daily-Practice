cb61363a09394fad52f97d32ce09ec2318b6a418
Kevyn Jesus <kevyn.contacts@gmail.com>
2023-12-29 11:36:51 -0300

    exercices Dec29

---
+++ Retorno de Valores.js
@@ -0,0 +1,13 @@
+/*
+
+Crie uma função chamada media que aceita uma lista de números como parâmetro e retorna a média desses números. 
+Teste a função com diferentes listas.
+
+*/
+
+function media(num1, num2, num3, num4, num5, num6){
+    let somaResult = num1 + num2 + num3 + num4 + num5 + num6;
+     console.log (somaResult / 6);
+}
+
+media(1,2,4,7,9,16)
