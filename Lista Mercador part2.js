59407559d8c614d706323cf919637089cf1e20bb
Kevyn Jesus <kevyn.contacts@gmail.com>
2024-01-11 14:48:20 -0300

    practice

---
+++ Lista Mercador part2.js
@@ -0,0 +1,18 @@
+ 
+ // Crie um array para representar uma lista de compras
+ let listaMercado = [
+    "Maça",
+    "Banana",
+    "Leite",
+    "Suco",
+ ];
+
+ // Adicione métodos para adicionar um item à lista
+ function adicionarItem(item){
+    listaMercado.push(item);
+ }
+
+ adicionarItem("Coco");
+
+ // Exiba todos os itens da lista
+ console.log(listaMercado);
