6a1bd580a073c6528086758d7948c2a7757969df
Kevyn Jesus <kevyn.contacts@gmail.com>
2024-01-16 14:53:41 -0300

    practice

---
+++ IntroDOM3.js
@@ -0,0 +1,10 @@
+function mostrar() {
+
+    // Passo 1: Seleção do elemento <h1>
+    let mostrarTitulo = document.querySelector("h1");
+
+    // Passo 2: Exibir o texto no console
+    console.log(mostrarTitulo.textContent);
+}
+
+mostrar();
