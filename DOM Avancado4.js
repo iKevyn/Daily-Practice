7b42d391723aa2fe506d6f5e544a56de90d667a6
Kevyn Jesus <kevyn.contacts@gmail.com>
2024-01-21 15:05:41 -0300

    update

--- DOM Avancado4.js
+++ DOM Avancado4.js
@@ -1,19 +1,27 @@
-/* 1. Crie um campo de entrada de texto para adicionar novas tarefas.
+/* 
+1. Crie um campo de entrada de texto para adicionar novas tarefas.
 2. Adicione um botão para adicionar a tarefa à lista.
 3. Exiba as tarefas em uma lista na página.
-4. Adicione um botão ou uma checkbox junto a cada tarefa para marcar como concluída.
-5. Adicione um botão para remover tarefas concluídas. */
+ */
 
-// / Criacao de um campo de entrada de texto para adicionar novas tarefas.
-let campoText = document.createElement("input");
-campoText.placeholder = "Adicionar";
-document.body.appendChild(campoText);
+// Criação de um campo de entrada de texto para adicionar novas tarefas.
+let campoTexto = document.createElement("input");  // Cria um novo elemento de entrada de texto.
+campoTexto.placeholder = "Adicionar";  // Define o texto do placeholder.
+document.body.appendChild(campoTexto);  // Adiciona o campo de texto ao corpo do documento.
 
+// Adição de um botão para adicionar a tarefa à lista.
+let botaoAdicionarTarefa = document.createElement("button");  // Cria um novo botão.
+botaoAdicionarTarefa.innerText = "Adicionar tarefa";  // Define o texto do botão.
+document.body.appendChild(botaoAdicionarTarefa);  // Adiciona o botão ao corpo do documento.
 
-//  Adicao de um botão para adicionar a tarefa à lista.
-let botaoAdicionarTarefa = document.createElement("button");
-botaoAdicionarTarefa.value = "Adicionar tarefa";
-botaoAdicionarTarefa.innerText = "Adicionar tarefa"
-document.body.appendChild(botaoAdicionarTarefa);
+// Criação de uma lista para exibir as tarefas.
+let listaTarefas = document.createElement("ul");  // Cria uma nova lista.
+document.body.appendChild(listaTarefas);  // Adiciona a lista ao corpo do documento.
 
-//  Exibição das tarefas em uma lista na página.
+// Função para adicionar tarefa à lista.
+botaoAdicionarTarefa.addEventListener("click", function(){
+    let tarefa = document.createElement("li");  // Cria um novo item de lista.
+    tarefa.innerText = campoTexto.value;  // Define o texto do item da lista como o valor do campo de texto.
+    listaTarefas.appendChild(tarefa);  // Adiciona o item da lista à lista de tarefas.
+    campoTexto.value = "";  // Limpa o campo de texto.
+});
